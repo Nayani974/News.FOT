044d3faa72235b7d3987b6ce697797a43d42aad9
Nayani974 <wickramaarachchinayani@gmail.com>
2025-06-11 06:25:06 +0530

    App_Clicked News Screen

---
+++ app/src/main/java/com/example/newsfot/ClickedNewsActivity.java
@@ -0,0 +1,28 @@
+package com.example.newsfot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClickedNewsActivity extends AppCompatActivity {
+
+    ImageView backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicked_news);
+
+        backBtn = findViewById(R.id.backBtn);
+
+        // When back is clicked, go back to News screen
+        backBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(ClickedNewsActivity.this, NewsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
